a0d1d857e4d6a2636165976decb641d4ce92736c
lcs-ldavidson <32200378+lcs-ldavidson@users.noreply.github.com>
2020-05-29 20:41:57 -0400

    Add files via upload

---
+++ DeltesianTranslator/DeltesianTranslator/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  DeltesianTranslator
+//
+//  Created by Liam Davidson on 2020-05-29.
+//  Copyright © 2020 Liam Davidson. All rights reserved.
+//
+
+import Foundation
+
+
+//testing how to do user input
+print("Hello, World!")
+var inputTest = "value"
+inputTest = readLine()!
+print("You have typed \(inputTest)")
+
